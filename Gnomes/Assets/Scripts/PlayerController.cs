5a726494b38024f6b19fe4a2202f1d4c0bb93223
zwonsey <zwonsey1@emich.edu>
2015-11-11 16:01:40 -0500

    /r/

---
+++ Gnomes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	static public PlayerController player;
+    public float Speed = 10f;
+    private float movex = 0f;
+	private int jump_count = 1;
+	public bool ____________________________;
+	public Rigidbody2D rb;
+    //private float movey = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+		player = this;
+        //change gravity value
+        Physics.gravity = new Vector3(0, -15.0f, 0);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //A to move left
+        if (Input.GetKey(KeyCode.A))
+        {
+            movex = -1;
+            //D to move right
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            movex = 1;
+        }
+        else
+        {
+            //player keeps moving if in air, otherwise stops
+            if (rb.velocity.y == 0)
+            {
+                movex = 0;
+            }
+            //movey = 0;
+        }
+        //jump if not already in air
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
+        {
+            rb.AddForce(new Vector2(0, 400));
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        //movex = Input.GetAxis ("Horizontal");
+        //movey = Input.GetAxis ("Vertical");
+        rb.velocity = new Vector2(movex * Speed, rb.velocity.y);
+        //rb.velocity = movex * Speed;
+    }
+}
