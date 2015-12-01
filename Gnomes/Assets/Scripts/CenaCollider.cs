3445180bc497578f90f46e207f826e92225c52c4
Richard Kalvaitis <rkalvait@emich.edu>
2015-11-30 19:02:18 -0500

    bugs

---
+++ Gnomes/Assets/Scripts/CenaCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenaCollider : MonoBehaviour {
+
+
+	public GameObject imagined;
+	public GameObject cena;
+	
+	public void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.tag == "Player") {
+			imagined.GetComponent<AudioSource>().Stop ();
+			cena.GetComponent<AudioSource>().Play();
+		}
+	}
+}
